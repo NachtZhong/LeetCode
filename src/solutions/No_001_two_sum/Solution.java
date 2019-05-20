3efe691de338769e224ea9cbdce3b9152cf2c40b
zhongpc@gzsendi.cn <389092126@qq.com>
2019-05-20 17:19:59 +0800

    added solutions for problem #1,#2,#122

---
+++ src/solutions/No_001_two_sum/Solution.java
@@ -0,0 +1,31 @@
+package solutions.No_001_two_sum;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ * @author Nacht
+ * Created on 2019/5/15
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 解题思路:
+ * 两个for循环遍历数组索引即可。
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] t = new int[2];
+        for(int a = 0 ; a < nums.length ; a ++){
+            for(int b = a + 1 ; b < nums.length ; b++){
+                if(nums[a] + nums[b] == target){
+                    t[0] = a ;
+                    t[1] = b ;
+                }
+            }
+        }
+        return t;
+    }
+}
