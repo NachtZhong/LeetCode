956272280a9023801055b831bd39e4534fdb1791
zhongpc@gzsendi.cn <389092126@qq.com>
2019-06-17 10:54:01 +0800

    problem #167 #682 #905 #575

--- src/solutions/No_0167_two_sum_2/Solution.java
+++ src/solutions/No_0167_two_sum_2/Solution.java
@@ -16,7 +16,25 @@ package solutions.No_0167_two_sum_2;
  * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * 解题思路:
- *
+ * 由于是已经排好序的数组, 从两边向中间检索
  */
 public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] resultArray = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                break;
+            }
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        resultArray[0] = left + 1;
+        resultArray[1] = right + 1;
+        return resultArray;
+    }
 }
